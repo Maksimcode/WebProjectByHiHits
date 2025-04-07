e6bc975aeed087e17d9acec10ea88c24ebfa3f6e
Olesya <lyzhinaolesya56@gmail.com>
2025-04-07 15:48:25 +0700

    update one more

--- Clusterization/app.js
+++ Clusterization/app.js
@@ -1,41 +1,9 @@
-function createGrid() {
-  const ROWS = 50;
-  const COLS = 50;
-  const container = document.getElementById("gridContainer");
-  container.innerHTML = "";
+const canvas = document.getElementById("canvas");
+const context = canvas.getContext("2d");
 
-  let grid = [];
-
-  for (let row = 0; row < ROWS; row++) {
-    const rowElement = document.createElement("div");
-    rowElement.className = "row";
-
-    let rowArray = [];
-
-    for (let col = 0; col < COLS; col++) {
-      const cell = document.createElement("div");
-      cell.className = "cell";
-      cell.dataset.row = row;
-      cell.dataset.col = col;
-      cell.addEventListener("mouseover", handleCellHover);
-      cell.addEventListener("click", handleCellClick);
-      rowElement.appendChild(cell);
-      rowArray.push(cell);
-    }
-
-    container.appendChild(rowElement);
-    grid.push(rowArray);
-  }
-}
-
-function handleCellHover(event) {
-  const cell = event.target;
-  // cell.classList.add("selected");
-}
-
-function handleCellClick(event) {
-  const cell = event.target;
-  cell.classList.add("selected");
-}
-
-createGrid();
+//базовая кластеризация
+let keys = [];
+let mstSet = [];
+let tree = [];
+let graph = [];
+let INF = 900000000;
